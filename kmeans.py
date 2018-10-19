a8ed9b8d3b8af6753fc9b12a89b51663a7613bef
Ryan <rhoang@gmu.edu>
2018-10-19 15:56:33 -0400

    initial commit

---
+++ kmeans.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+# start Main------------------------------------------------------------
+if __name__ == '__main__':
+    
+    inputDataRaw = []
+    inputDataProcessed = []
+    
