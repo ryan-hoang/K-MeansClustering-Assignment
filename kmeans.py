d9e484c19e3ebc743d2709552c2c32666ad4f6fd
ryan1246 <rhoang@gmu.edu>
2018-10-20 19:35:19 -0400

    ready to test text

--- kmeans.py
+++ kmeans.py
@@ -5,61 +5,76 @@ import pickle
 import scipy.sparse as sc
 
 def cluster(vectors, k,centroids):
-    vectors = np.array(vectors)
-    clusterLabels = [-1]*len(vectors)
+    clusterLabels = [-1]*(vectors.get_shape()[0])
     #print(clusterLabels)
-    for count,x in enumerate(vectors):
+    for count in range(vectors.get_shape()[0]):
+        x = vectors.getrow(count).toarray()[0]
         dists = []
+        #print("centroids: {} iteration:{}", centroids, count)
         for c in centroids:
             dists.append(1 - (np.dot(x,c) / (np.linalg.norm(x) * np.linalg.norm(c)))) # list of distances from x to every centroid (cosine distance)
         min = 0
         for i in range(k):
+            #print(i)
             if dists[i] < dists[min]:
                 min = i
         clusterLabels[count] = min+1
-    
+
     #recalculate clusters now
-    
+
     listofclusters = []
     for i in range(0,k):
         listofclusters.append([])
-    
-    for i in range(len(vectors)): # populate list of clusters with vectors 
-        listofclusters[clusterLabels[i]-1].append(vectors[i])
-    
+
+    for i in range(vectors.get_shape()[0]): # populate list of clusters with vectors
+        #print(type(vectors.getrow(i).toarray()), vectors.getrow(i).toarray()[0])
+        listofclusters[clusterLabels[i]-1].append(vectors.getrow(i).toarray()[0])
+
     updatedCentroids = []
-    print(len(listofclusters))
+    #print(len(listofclusters))
     for cluster in listofclusters: # calculate mean vector in each cluster set that to the centroid
         updatedCentroids.append(np.mean(cluster, axis=0))
-    
+
     return clusterLabels,np.array(updatedCentroids)
-    
+
 def initCentroids(vectors, k):
     centroids = []
-    centroids.append(np.array(random.sample(vectors,1)[0]))
-    vectors = np.array(vectors)
+    randomnumber = random.randint(0,vectors.get_shape()[0])
+    #print(randomnumber)
+    t = vectors.getrow(randomnumber)
+    #print("t: ",t.toarray())
+    centroids.append(t.toarray()[0].tolist())
+    #print("Centroids:", centroids)
+
     for i in range(1,k):
+        #print("Centroids: ",centroids)
+        #print("Centroids:{} Iteration: {}".format(centroids,i))
         squaredDists = getDistances(vectors, centroids)
         rand = random.random()
         cumulativeProbabilities = (squaredDists/squaredDists.sum()).cumsum()
-        centroids.append(vectors[np.where(cumulativeProbabilities >= rand)[0][0]])
-    return np.array(centroids)
-    
+        blah = vectors.getrow(np.where(cumulativeProbabilities >= rand)[0][0]).toarray()
+        #print(type(blah))
+        centroids.append(blah.tolist()[0])
+    #print("Centroids returned by initCentroids:", centroids)
+    return centroids
+
 def getDistances(X,cent):
-    vectors = sc.csr_matrix(X,copy = True)
-    cen = sc.csr_matrix(cent,copy=True)
+    vectors = X
+    #print("Cent in getDistances:", cent)
+    cen = sc.dok_matrix(cent)
     dist = []
     temp = []
     for indx in range(vectors.get_shape()[0]):
         x = vectors.getrow(indx).toarray()[0]
         for indc in range(cen.get_shape()[0]):
-            c = cen.getrow(indc).toarray()[0]
+            c = cen.getrow(indc).toarray()
+            #print(x,"c: ",c)
             temp.append(np.linalg.norm(np.subtract(x,c))**2)
         dist.append(np.array(temp).min())
         temp = []
 
     return np.array(dist)
-    
+
 def processData():
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
@@ -76,13 +91,13 @@ def processData():
     print("Mode: {}".format(mode))
     inputDataRaw = []
     inputDataProcessed = []
-    
+
     with open(inputFile) as file:
         for line in file:
             inputDataRaw.append(line) # each line of input is one element of inputDataRaw
-    
+
     if mode == "text":
-        inputDataProcessed = sc.csr_matrix((8550,126373),dtype=np.int8)
+        inputDataProcessed = sc.dok_matrix((8580,126373),dtype=np.int8)
         for ind,line in enumerate(inputDataRaw):
             splitLine = line.split()
             iterations = int(len(splitLine)/2)
@@ -97,28 +112,29 @@ def processData():
                 del splitLine[0]
                 inputDataProcessed[ind,index-1] = count
             #inputDataProcessed.append(vector)
-        print("Data loaded. {} data vectors loaded.\n".format(len(inputDataProcessed)))
+        print("Data loaded. {} data vectors loaded.\n".format(len(inputDataRaw)))
         pickle.dump(inputDataProcessed,open("textCSRSparse.dat",'wb'))
     else:
-        for line in inputDataRaw:
+        inputDataProcessed = sc.dok_matrix((150,4),dtype=np.float64)
+        for row,line in enumerate(inputDataRaw):
             splitLine = line.split()
-            vector = []
-            for item in splitLine:
-                vector.append(float(item))
-            inputDataProcessed.append(np.array(vector))
-        print("Data loaded. {} data vectors loaded.\n".format(len(inputDataProcessed)))
+            for col,item in enumerate(splitLine):
+                #print(item,type(float(item)))
+                inputDataProcessed[row, col] = float(item)
+        print("Data loaded. {} data vectors loaded.\n".format(len(inputDataRaw)))
+        #print(inputDataProcessed.toarray())
     return inputDataProcessed
-    
+
 def generateAnswerFile(labels):
     with open("clusterLabels.data", 'w') as outFile:
         for item in labels:
             outFile.write("{}\n".format(item))
 # start Main------------------------------------------------------------
 if __name__ == '__main__':
-    k = 3
+    k = 7
     vectors = processData()
     centroids = initCentroids(vectors,k)
-    
+
     while True:
         assignments,newCentroids = cluster(vectors,k,centroids)
         if(np.array_equal(centroids,newCentroids)):
